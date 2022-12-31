f92016cc9040cac6bef190dd66bc6bdb25609e0c
JakFranceschini <jakson.franceschini@gmail.com>
2022-12-31 15:53:30 -0300

    adicionado menu hamburguer e melhorias gerais

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,21 +1,70 @@
 import './TopBar.css'
-import React from "react";
-import ImgTopBar from "../Img/imgTopBar.jpg";
+import React from 'react'
+import { useEffect } from 'react'
+import ImgTopBar from '../Img/imgTopBar.jpg'
+import { FiMenu, FiX } from 'react-icons/fi'
 
-export default props =>
+function TopBar(){
 
-    <div className="topBar">
+    useEffect(() => {
+        let iconeHamburguer = document.getElementById('iconeHamburguer')
+        let iconeX = document.getElementById('iconeX')
+        let menuHamburguer = document.getElementById('menuHamburguer')
+        let containerImgNome = document.getElementById('containerImgNome')
+        let menuHamburguerLinks = document.getElementById('menuHamburguerLinks')
+    
+        iconeHamburguer.addEventListener('click', function mostrarMenu(){
+        containerImgNome.style.display = 'none'
+        menuHamburguer.style.display = 'flex'
+        iconeHamburguer.style.display = 'none'
+        menuHamburguer.appendChild(menuHamburguerLinks)
+        menuHamburguerLinks.style.display = 'flex'
+        iconeX.style.display = 'flex'
+        });
+    });
 
-        <div className="containerImgNome">
-            <a href="/"><img id="imgTopBar" src={ImgTopBar}></img></a>
-            <h2 id="nomeTopBar"></h2>
-        </div>
-
-        <ul className="menuTopBar">
-            <li><a href="/">Home</a></li>
-            <li><a href="Sobre">Sobre</a></li>
-            <li><a href="Projetos">Projetos</a></li>
-            <li><a className="linkSkills" href="#containerSkills">Skills</a></li>
-        </ul>
+    useEffect(() => {
+        let iconeX = document.getElementById('iconeX')
+        let menuHamburguer = document.getElementById('menuHamburguer')
+        let containerImgNome = document.getElementById('containerImgNome')
+        let iconeHamburguer = document.getElementById('iconeHamburguer')
         
-    </div>
+        iconeX.addEventListener('click', function ocultarMenu(){
+            menuHamburguer.style.display = 'none'
+            containerImgNome.style.display = 'flex'
+            iconeHamburguer.style.display = 'flex'
+        });
+    });
+
+    return (
+        <div className="topBar">
+
+            <div id="containerImgNome">
+                <a href="/"><img id="imgTopBar" src={ImgTopBar}></img></a>
+                <h2 id="nomeTopBar"></h2>
+            </div>
+
+            <ul className="menuTopBar">
+                <li><a href="/">Home</a></li>
+                <li><a href="Sobre">Sobre</a></li>
+                <li><a href="Projetos">Projetos</a></li>
+                <li><a href="#containerSkills">Skills</a></li>
+            </ul>
+
+            <FiMenu id="iconeHamburguer" size={60} color="#f5f5f5"/>
+
+            <div id="menuHamburguer"></div>
+
+            <ul id="menuHamburguerLinks">
+                <li><a href="/">Home</a></li>
+                <li><a href="Sobre">Sobre</a></li>
+                <li><a href="Projetos">Projetos</a></li>
+                <li><a href="#containerSkills">Skills</a></li>
+                <li><FiX id="iconeX" color="#f5f5f5"/></li>
+            </ul>
+        
+        </div>
+    )
+}
+
+export default TopBar
