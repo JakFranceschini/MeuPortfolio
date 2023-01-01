fd857c775d6ef0ff4e67f0e5ef2f127237edae81
JakFranceschini <jakson.franceschini@gmail.com>
2023-01-01 15:51:19 -0300

    Adicionado itens na pagina projetos e melhorias em geral

--- src/Pages/Projetos/PaginaProjetos.jsx
+++ src/Pages/Projetos/PaginaProjetos.jsx
@@ -1,6 +1,8 @@
 import './PaginaProjetos.css'
-import TopBar from "../../Components/TopBar";
-import Footer from '../../Components/Footer';
+import TopBar from '../../Components/TopBar'
+import Footer from '../../Components/Footer'
+import ImgPrimeiroCard from './../../Img/imgPrimeiroCard.png'
+import { BsGithub, BsFillArrowRightCircleFill } from 'react-icons/bs'
 
 function PaginaProjetos() {
 
@@ -9,8 +11,110 @@ function PaginaProjetos() {
 
             <TopBar/>
 
-            <div className='container'>
-                
+            <div className='containerProjetos'>
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
+
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
+
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
+
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
+
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
+
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
+
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
+
+                <div className='cardProjeto'>
+                    <h2>Profile Card</h2>
+                    <img src={ImgPrimeiroCard}></img>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                    </p>
+                    <a href='https://github.com/JakFranceschini/ProfileCard' target='blank'>
+                        <BsGithub size={40} color="#f5f5f5"/>
+                        <BsFillArrowRightCircleFill className='seta' size={25} color="#f5f5f5"/>
+                    </a>
+                </div>
             </div>
 
             <Footer/>
